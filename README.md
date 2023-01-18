1d89d261938a51b0c93ce23a99a1ef4b61297f8d
cbstl <chris.brown.stl@gmail.com>
2023-01-17 22:04:08 -0600

    base case - 2 albums, no axios

--- README.md
+++ README.md
@@ -1,43 +1,68 @@
-### Objective
+# dj-japan-rabbit-embbri
 
-White Rabbit Japan is branching into the music business and needs a new website. Build it using TypeScript and Vue.
+This template should help get you started developing with Vue 3 in Vite.
 
-### Brief
+## Recommended IDE Setup
 
-In a fictional world, White Rabbit Japan is branching into the music business and we need a new a website. This website needs to display the top 100 songs based on the iTunes API. This code challenge allows you to choose your own path and lets you flaunt your creative panache and technical skills along the way.
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-### Tasks
+## Type Support for `.vue` Imports in TS
 
--   Implement assignment using:
-    -   Language: **TypeScript**
-    -   Framework: **Vue**
--   Show top 100 albums based on the json feed here: `https://itunes.apple.com/us/rss/topalbums/limit=100/json`
--   Cross browser support (or graceful degradation)
--   A clean modern look and responsive design
--   A good user experience
--   Use a CSS Framework (Bootstrap, Foundation, Pure, etc.…)
--   Allow the top 100 to be searchable
--   Surprise us! Add a feature that you think would work well here (for instance, advanced search, integration with other API, a "Favorite" functionality)
-    -   Describe the feature in separate markdown file
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.
 
-### Deliverables
+If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:
 
-Make sure to include all source code in the repository. 
+1. Disable the built-in TypeScript Extension
+    1) Run `Extensions: Show Built-in Extensions` from VSCode's command palette
+    2) Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
+2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
 
-### Evaluation Criteria
+## Customize configuration
 
--   **TypeScript** best practices
--   We're looking for you to produce working code, with enough room to demonstrate how to structure components in a small program.
--   Show us your work through your commit history
--   Completeness: did you complete the features?
--   Correctness: does the functionality act in sensible, thought-out ways?
--   Maintainability: is it written in a clean, maintainable way?
--   Testing: is the system adequately tested?
+See [Vite Configuration Reference](https://vitejs.dev/config/).
 
-### CodeSubmit
+## Project Setup
 
-Please organize, design, test and document your code as if it were going into production - then push your changes to the master branch. After you have pushed your code, you may submit the assignment on the assignment page.
+```sh
+npm install
+```
 
-All the best and happy coding,
+### Compile and Hot-Reload for Development
 
-The White Rabbit Japan Team
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Run Unit Tests with [Vitest](https://vitest.dev/)
+
+```sh
+npm run test:unit
+```
+
+### Run End-to-End Tests with [Cypress](https://www.cypress.io/)
+
+```sh
+npm run test:e2e:dev
+```
+
+This runs the end-to-end tests against the Vite development server.
+It is much faster than the production build.
+
+But it's still recommended to test the production build with `test:e2e` before deploying (e.g. in CI environments):
+
+```sh
+npm run build
+npm run test:e2e
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,188 @@
+<script setup lang="ts">
+  //import axios from 'axios'
+
+  const albums = [
+    {
+      "im:name": {
+          "label": "Episode 41 - Thor’s Angels"
+      },
+      "im:image": [
+          {
+              "label": "https://is2-ssl.mzstatic.com/image/thumb/Music113/v4/46/04/15/46041510-d51e-a612-adf1-f1b2ae8be388/859714807661_cover.jpg/55x55bb.png",
+              "attributes": {
+                  "height": "55"
+              }
+          },
+          {
+              "label": "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/46/04/15/46041510-d51e-a612-adf1-f1b2ae8be388/859714807661_cover.jpg/60x60bb.png",
+              "attributes": {
+                  "height": "60"
+              }
+          },
+          {
+              "label": "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/46/04/15/46041510-d51e-a612-adf1-f1b2ae8be388/859714807661_cover.jpg/170x170bb.png",
+              "attributes": {
+                  "height": "170"
+              }
+          }
+      ],
+      "im:itemCount": {
+          "label": "1"
+      },
+      "im:price": {
+          "label": "$2.99",
+          "attributes": {
+              "amount": "2.99",
+              "currency": "USD"
+          }
+      },
+      "im:contentType": {
+          "im:contentType": {
+              "attributes": {
+                  "term": "Album",
+                  "label": "Album"
+              }
+          },
+          "attributes": {
+              "term": "Music",
+              "label": "Music"
+          }
+      },
+      "rights": {
+          "label": "℗ 2012 Dan Carlin"
+      },
+      "title": {
+          "label": "Episode 41 - Thor’s Angels - Dan Carlin"
+      },
+      "link": {
+          "attributes": {
+              "rel": "alternate",
+              "type": "text/html",
+              "href": "https://music.apple.com/us/album/episode-41-thors-angels/995162070?uo=2"
+          }
+      },
+      "id": {
+          "label": "https://music.apple.com/us/album/episode-41-thors-angels/995162070?uo=2",
+          "attributes": {
+              "im:id": "995162070"
+          }
+      },
+      "im:artist": {
+          "label": "Dan Carlin",
+          "attributes": {
+              "href": "https://music.apple.com/us/artist/dan-carlin/75845963?uo=2"
+          }
+      },
+      "category": {
+          "attributes": {
+              "im:id": "50000061",
+              "term": "Spoken Word",
+              "scheme": "https://music.apple.com/us/genre/music-spoken-word/id50000061?uo=2",
+              "label": "Spoken Word"
+          }
+      },
+      "im:releaseDate": {
+          "label": "2012-01-19T00:00:00-07:00",
+          "attributes": {
+              "label": "January 19, 2012"
+          }
+      }
+  },
+  {
+      "im:name": {
+          "label": "Memory Lane (Sampler) - EP"
+      },
+      "im:image": [
+          {
+              "label": "https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/e3/88/57/e3885745-dda5-ad82-1e5b-fd0dcba33852/196589774057.jpg/55x55bb.png",
+              "attributes": {
+                  "height": "55"
+              }
+          },
+          {
+              "label": "https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/e3/88/57/e3885745-dda5-ad82-1e5b-fd0dcba33852/196589774057.jpg/60x60bb.png",
+              "attributes": {
+                  "height": "60"
+              }
+          },
+          {
+              "label": "https://is4-ssl.mzstatic.com/image/thumb/Music123/v4/e3/88/57/e3885745-dda5-ad82-1e5b-fd0dcba33852/196589774057.jpg/170x170bb.png",
+              "attributes": {
+                  "height": "170"
+              }
+          }
+      ],
+      "im:itemCount": {
+          "label": "4"
+      },
+      "im:price": {
+          "label": "$3.49",
+          "attributes": {
+              "amount": "3.49",
+              "currency": "USD"
+          }
+      },
+      "im:contentType": {
+          "im:contentType": {
+              "attributes": {
+                  "term": "Album",
+                  "label": "Album"
+              }
+          },
+          "attributes": {
+              "term": "Music",
+              "label": "Music"
+          }
+      },
+      "rights": {
+          "label": "℗ 2023 Sony Music Entertainment. All rights reserved."
+      },
+      "title": {
+          "label": "Memory Lane (Sampler) - EP - Old Dominion"
+      },
+      "link": {
+          "attributes": {
+              "rel": "alternate",
+              "type": "text/html",
+              "href": "https://music.apple.com/us/album/memory-lane-sampler-ep/1662661465?uo=2"
+          }
+      },
+      "id": {
+          "label": "https://music.apple.com/us/album/memory-lane-sampler-ep/1662661465?uo=2",
+          "attributes": {
+              "im:id": "1662661465"
+          }
+      },
+      "im:artist": {
+          "label": "Old Dominion",
+          "attributes": {
+              "href": "https://music.apple.com/us/artist/old-dominion/495761008?uo=2"
+          }
+      },
+      "category": {
+          "attributes": {
+              "im:id": "6",
+              "term": "Country",
+              "scheme": "https://music.apple.com/us/genre/music-country/id6?uo=2",
+              "label": "Country"
+          }
+      },
+      "im:releaseDate": {
+          "label": "2023-01-09T00:00:00-07:00",
+          "attributes": {
+              "label": "January 9, 2023"
+          }
+      }
+  }
+  ]
+</script>
+
+<template>
+  <main>
+    <div>
+      <li v-for="album in albums">
+        <p>{{album.title.label}} by {{album["im:artist"].label}}</p>
+      </li>
+    </div>
+  </main>
+</template>
